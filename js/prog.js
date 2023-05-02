03bbaf27e690aed1fc81bfd36524eb95f8d518cd
dileep-maram <dileepmaram07@gmail.com>
2023-05-02 20:08:40 +0530

    Added project

---
+++ js/prog.js
@@ -0,0 +1,76 @@
+/*var age = 30;
+if(age < 31){
+	console.log("age is less than 30");
+}
+else{
+	console.log("age is greater than 30");
+}
+
+for(var i = 1; i <= 50; i++){
+	console.log("print 1 to 50 numbers:",i);
+}
+
+for(var i = 10; i<=20; i++){
+	console.log("print 10 to 20 numbers:",i);
+}
+
+for(var i = 50; i <= 100; i++){
+	if((i%2) === 0){
+		console.log("print even nuumbers between 50 to 100 numbers:",i);
+	}
+}
+
+for(var i = 100; i >= 1; i--){
+	console.log("print numbers in rev from 100 to 1:",i);
+}
+
+for(var i = 91; i <= 133; i++){
+	if((i%9) === 0){
+		console.log("multiples of 9 from 91 to 133",i);
+	}
+}
+
+for(var i = 500; i <= 550; i++){
+	if((i%2) != 0){
+		console.log("odd numbers from 500 to 550:",i);
+	}
+}
+
+var employees = [
+	{
+		name : "dil",
+		cell : 7702210507,
+		Email: "maramdileep2@gmail.com"
+	},
+	{
+		name : "bramaih",
+		cell : 8970215255,
+		Email: "bachu@gmail.com"
+	},
+	{
+		name : "nagendra",
+		cell : 9885921762,
+		Email: "nagendra76@gmail.com"
+	},
+	{
+		name : "jyoshna",
+		cell : 8885423142,
+		Email: "maramdileep2@gmail.com"
+	},
+	{
+		name : "venkatesh",
+		cell : 6302766542,
+		Email: "venkateshv3@gmail.com"
+	}
+];
+console.log("Employee details:",employees);*/
+
+function demo(){
+	var a = 4;
+	var b = "5";
+	var c = 35;
+	var d = "7";
+	var result = (a + c + d + b);
+	var result1 = (a + c + b);
+	console.log(typeof(result1));
+};
